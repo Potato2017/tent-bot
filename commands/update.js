766ce53c14f717da3e4644125a4ab3e03d757c7e
Potato2017 <70041516+Potato2017@users.noreply.github.com>
2022-01-12 16:49:47 -0800

    update

--- commands/update.js
+++ commands/update.js
@@ -8,7 +8,7 @@ module.exports = {
 		.setName('update')
 		.setDescription('updates the bot - for meeeeee'),
 	async execute(interaction) {
-		if (interaction.user.id !== 439888132435869706) {
+		if (interaction.user.id !== '439888132435869706') {
 			await interaction.reply( {content: 'imagine trying to restart but not having permissions', ephemeral: true});
 			return;
 		}
