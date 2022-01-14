b2ac6adf2c909b44f25b62823790321f88fa7e77
potato2017 <70041516+Potato2017@users.noreply.github.com>
2022-01-13 21:37:53 -0800

    itemlb store userlist as variable, add items.json to gitignore

--- commands/itemlb.js
+++ commands/itemlb.js
@@ -10,12 +10,13 @@ module.exports = {
             }
             try {
                 const mydata = JSON.parse(jsonString);
-                var page = interaction.options.getInteger('page');
-                if ((page-1)*20 > Object.keys(mydata.users).length) {
-                    await interaction.reply(`that's too high, try a number between 1 and ${Math.ceil(Object.keys(mydata.users).length/20)}`);
+                const page = interaction.options.getInteger('page');
+                const userList = Object.keys(mydata.users);
+                if ((page-1)*20 > userList.length) {
+                    await interaction.reply(`that's too high, try a number between 1 and ${Math.ceil(userList.length/20)}`);
                     return
                 } else if (page < 0) {
-                    await interaction.reply(`that's too low, try a number between 1 and ${Math.ceil(Object.keys(mydata.users).length/20)}`);
+                    await interaction.reply(`that's too low, try a number between 1 and ${Math.ceil(userList.length/20)}`);
                     return
                 }
                 if (page === null) {
@@ -23,11 +24,11 @@ module.exports = {
                 }
                 var totals = {};
                 var output = '';
-                for (var i = 0; i < Object.keys(mydata.users).length; i++) {
-                    totals[Object.keys(mydata.users)[i]] = 0;
-                    var allvals = Object.values(mydata.users[Object.keys(mydata.users)[i]].items);
+                for (var i = 0; i < userList.length; i++) {
+                    totals[userList[i]] = 0;
+                    var allvals = Object.values(mydata.users[userList[i]].items);
                     for (var j = 0; j < allvals.length; j++) {
-                        totals[Object.keys(mydata.users)[i]] += allvals[j]
+                        totals[userList[i]] += allvals[j]
                     }
                     
                 }
@@ -36,7 +37,7 @@ module.exports = {
                     let usr = await interaction.client.users.fetch(entries[i][0]);
                     output += `${usr.tag} - ${entries[i][1]}\n`
                 }
-                output += `page ${page} of ${Math.ceil(Object.keys(mydata.users).length/20)}`;
+                output += `page ${page} of ${Math.ceil(userList.length/20)}`;
                 await interaction.reply(output)
             
             } catch(err) {
